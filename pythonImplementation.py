a18686e886f39ae70e70361009f6c90168df95e0
parthtyagi27 <44475459+parthtyagi27@users.noreply.github.com>
2018-11-27 16:56:29 -0600

    Update pythonImplementation.py
    Developed the decrypt function

--- pythonImplementation.py
+++ pythonImplementation.py
@@ -20,16 +20,38 @@ def encrypt(key, text):
         if counter >= len(keyArray):
             counter = 0;
         position = getPos(letter)
-        print("Shifting " + str(position) + " with " + str(keyArray[counter]))
         position += keyArray[counter]
         if position > len(alphabet):
             position = position % len(alphabet)
+        print("Shifting " + str(position) + " with " + str(keyArray[counter]) + " to " + str(position))
         counter += 1
         encryptedString += alphabet[position]
         
     print(keyArray)
     return encryptedString;
 
+def decrypt(key, text):
+    key = list(key)
+    text = list(text)
+    keyArray = []
+    decryptedString = ""
+    for index, letter in enumerate(key):
+        keyArray.insert(index, getPos(letter))
+    counter = 0
+    for index, letter in enumerate(text):
+        if counter >= len(keyArray):
+            counter = 0;
+        position = getPos(letter)
+        position = position - keyArray[counter]
+        if position < 0:
+            position = position + len(alphabet)
+        print("Shifting " + str(position) + " with " + str(keyArray[counter]) + " to " + str(position))
+        counter += 1
+        decryptedString += alphabet[position]
+        
+    print(keyArray)
+    return decryptedString;
+
+
 print(encrypt("hey", "yeah"))
-
-
+print(decrypt("hey", "fiyo"))
